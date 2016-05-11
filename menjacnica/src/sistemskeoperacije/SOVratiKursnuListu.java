c0815843829a2bf62fe5052719d6aebe59fe5651
miladavuk <milada.vukovic@gmail.com>
2016-05-11 14:51:41 +0200

    Za sve metode iz klase Menjacnica napravljene klase za sistemske operacije u paketu sistemskeoperacije.

---
+++ menjacnica/src/sistemskeoperacije/SOVratiKursnuListu.java
@@ -0,0 +1,13 @@
+package sistemskeoperacije;
+
+import java.util.LinkedList;
+
+import menjacnica.Valuta;
+
+public class SOVratiKursnuListu {
+	
+	public static LinkedList<Valuta> izvrsi(LinkedList<Valuta> kursnaLista) {
+		return kursnaLista;
+	}
+
+}
